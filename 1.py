fda2b72684759427984c640081f2e426fe444667
Christian Løverås <cl@superelectric.net>
2018-12-02 19:32:23 +0100

    Part two too

--- 1.py
+++ 1.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
+#
+# https://adventofcode.com/2018
 
-i = 0
+print ("What's the frequency, Kenneth?")
+
+# Read frequency changes from file into a changelist
+changelist = []
 with open('frequency-changes.txt') as file_:
     for line in file_:
-        i += int(line)
-print(i)
+        changelist.append(line)
+
+
+# Loop through changelist over and over, until reaching a previously seen frequency
+current = 0
+seen = {}
+found = False
+while not found:
+    for i in range(len(changelist)):
+        current += int(changelist[i])
+        if current in seen.keys():
+            found = True
+            break
+        else:
+            seen[current] = True
+
+print("It's", current)
