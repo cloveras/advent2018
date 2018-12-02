febc415b3f1612a8fa02aa5c1e28de6f0e3c1892
Christian Løverås <cl@superelectric.net>
2018-12-02 20:50:35 +0100

    Perl too

---
+++ 1.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+use strict;
+use File::Slurp;
+
+my @changes = read_file("frequency-changes.txt", chomp => 1);
+my $frequency = 0;
+my %seen = ();
+my $found = 0;
+
+while (! $found) {
+  foreach (@changes) {
+    $frequency += $_;
+    if ($seen{$frequency}) {
+      $found = 1;
+      last;
+    } else {
+      $seen{$frequency} = 1;
+    }
+  }
+}
+
+print "$frequency\n";
